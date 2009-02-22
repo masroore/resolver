1d0f6de2dbfdca1532f7a68f130be1f08ec52ac0
fuzzyman <fuzzyman@467c400e-6a35-0410-a56b-ffeb0b568cfb>
2009-02-22 01:32:50 +0000

    Changes to TestableSheet to use RunWorkbook with Resolver One 1.4. Not working yet unfortunately.
    Michael

--- trunk/TestableSheet/TestableSheetUnitTests.bat
+++
@@ -1,2 +0,0 @@
-bin\RSIronPython.exe TestableSheet\TestableSheetTest.py
-pause
---
+++ trunk/TestableSheet/runworkbook.py
@@ -0,0 +1,32 @@
+import clr
+import sys
+
+from System.IO import Directory, Path
+
+x86path = r'C:\Program Files\Resolver One\bin'
+x64path = r'C:\Program Files (x86)\Resolver One\bin'
+
+if Directory.Exists(x86path):
+    topLevel = Path.GetDirectoryName(x86path)
+    bin = x86path
+else:
+    topLevel = Path.GetDirectoryName(x64path)
+    bin = x64path
+
+Directory.SetCurrentDirectory(topLevel)
+sys.path.extend((topLevel, bin))
+
+# This adds references to all .NET assemblies
+# that Resolver One spreadsheets use
+clr.AddReference('TopLevel')
+import LoadRequiredAssemblies
+
+
+from Library.Workbook import Workbook
+from Library.ContextDependent import MakeContextDependentFunctions
+
+_workbook = Workbook()
+
+ContextDependentFunctions = MakeContextDependentFunctions(__file__, _workbook, sys)
+RunWorkbook = ContextDependentFunctions.RunWorkbook
+
